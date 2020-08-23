3f0a980fc4a4ebbbf652967eaa5dabf2be14f551
jamesmoriarity <james@moriarity.org>
2020-08-22 19:44:24 -1000

    removed unused files

--- src/importer.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './App';
-console.log('root = ' + document.getElementById('root'));
-render(React.createElement(App, null), document.getElementById('root'));
--- src/index.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import { render } from 'react-dom'
-import App from './App'
-console.log('root = ' + document.getElementById('root'))
-render(<App />, document.getElementById('root'))
